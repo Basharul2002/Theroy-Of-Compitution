212c055be1880f3af0f9057a9ed18e427edc5450
Basharul Alam Mazu <112147775+Basharul2002@users.noreply.github.com>
2024-03-18 21:40:39 +0600

    Create 2.cpp

---
+++ NFA/2.cpp
@@ -0,0 +1,66 @@
+/*
+Alpahbet = {0, 1]
+
+Language = {w|w is a bit string that ends with 1}
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+enum State
+{
+   START,
+   q0, 
+   q1 
+};
+
+
+bool isAccepted(State currentState) 
+{
+    return currentState == q1;
+}
+
+
+State processInput(State currentState, char input)
+{
+    if(input == '1')
+        return q1;
+    
+    else if(input == '0')
+        return q0;
+    
+    return q0;
+    
+}
+
+
+int main() 
+{
+    string input;
+    cout<<"Enter a bit string: ";
+    cin>>input;
+    State currentState = START;
+    
+    for(char c: input)
+    {
+        if(c != '0' && c != '1')
+        {
+            cout<<"Invalid Input\n";
+            return 0;
+        }
+        
+        currentState = processInput(currentState, c);
+    }
+
+    if (isAccepted(currentState)) 
+        cout<<"Accepted\n";
+    
+    
+    else 
+        cout<<"Rejected\n";
+    
+
+    return 0;
+}
