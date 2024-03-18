8b36a9ee270db371132641cecb0f6cb9117d0148
Basharul Alam Mazu <112147775+Basharul2002@users.noreply.github.com>
2024-03-18 13:41:05 +0600

    Create 3.cpp

---
+++ DFA/3.cpp
@@ -0,0 +1,66 @@
+// Alphabet = {0,1}
+// Language = {w|w is a bit string that starts with 0 and ends with 1}
+
+#include<bits/stdc++.h>
+using namespace std;
+
+enum State
+{
+    START,
+    ACCPECT,
+    REJECT
+};
+    
+State currentState;
+    
+
+        
+bool isAccpected(State currentState)
+{
+    return currentState == ACCPECT;
+}
+        
+
+State processInput(State currentState, char input)
+{
+    if(input == '0' && currentState == START)
+        return ACCPECT;
+    
+    else if(input == '1')
+        return ACCPECT;
+        
+    else
+        return REJECT;
+}
+
+
+int main()
+{
+    State currentState = START;
+    string input;
+    
+    cout<<"Enter the bit string: ";
+    cin>>input;
+    
+    for(char c: input)
+    {
+        if(c !='0' && c != '1')
+        {
+            cout<<"Invalid Input\n";
+            return 0;
+        }
+        
+        currentState = processInput(currentState, c);
+    }
+    
+    if(isAccpected(currentState))
+        cout<<"Accepted\n";
+    
+    else
+        cout<<"Rejected\n";
+
+    return 0;
+}
+
+
+    
