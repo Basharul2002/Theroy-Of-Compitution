8393e20838be566ca2062e6fc781c12712e8e9cb
Basharul Alam Mazu <112147775+Basharul2002@users.noreply.github.com>
2024-03-18 14:56:48 +0600

    Create 9.cpp

---
+++ DFA/9.cpp
@@ -0,0 +1,80 @@
+// Alphabet = {0,1}
+// Language = {w|w is a string that does not contain substring 101}
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+enum State
+{
+    FIRST_FOUND,
+    SECOND_FOUND,
+    ACCPECT, 
+    REJECT
+};
+    
+State currentState;
+    
+
+        
+bool isAccpected(State currentState)
+{
+    return currentState == ACCPECT;
+}
+        
+
+State processInput(State currentState, char input)
+{
+    if(input == '1' && currentState == REJECT)
+        return FIRST_FOUND;
+        
+    
+    else if(input == '0' && currentState == FIRST_FOUND)
+        return SECOND_FOUND;
+        
+    else if(input == '1' && currentState == SECOND_FOUND)
+        return ACCPECT;
+        
+        
+    else if(currentState == ACCPECT)
+            return ACCPECT;
+    
+    
+    else
+        return REJECT;
+}
+
+
+int main()
+{
+    string input;
+    int i = 0;
+    
+
+        State currentState = REJECT;
+    
+    cout<<"Enter the bit string: ";
+    cin>>input;
+    
+    
+    for(char c: input)
+    {
+        if(c !='0' && c != '1')
+        {
+            cout<<"Invalid Input\n";
+            return 0;
+        }
+        
+        currentState = processInput(currentState, c);
+    }
+    
+    // if 101 found then Rejected
+    if(isAccpected(currentState))
+        cout<<"Rejected\n";
+    
+    else
+        cout<<"Accpected\n";
+
+
+    return 0;
+}
